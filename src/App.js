e238052e10a6b196720fa0395a9022bdd6d05c6e
Ahsan-Ehtesham <ahsanehtesham110@gmail.com>
2021-10-11 01:10:26 +0500

    Initial-Commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import React from 'react';
+import React from "react";
 import Particles from "react-tsparticles";
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import particlesOptions from "./particles.json";
 
 function App() {
-    return (
-        <div className="App">
-            <Particles options={particlesOptions}/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
+  return (
+    <div className="App">
+      <Particles options={particlesOptions} />
+      <div className="flex-container">
+        <div className="row">
+          <div className="img-container flex-item">
+            <img
+              src="https://gravatar.com/avatar/a8d7b9b47f75adbd42c65a301c361e25?s=128&d=robohash&r=x"
+              alt="Avatar"
+            />
+          </div>
+
+          <h1 className="flex-item mt-3">Hi, I'm Ahsan <span>Ehtesham</span></h1>
+          <h2 className="flex-item mt-2">I'm a React Developer.</h2>
+          <div className="social mt-2">
+            <div className="card">
+              <div className="card-body">
+                <a href="https://github.com/Ahsan-Ehtesham">
+                  <i className="fab fa-github fa-3x"></i>
                 </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
+                <a href="https://github.com/Ahsan-Ehtesham">
+                  <i className="fab fa-facebook fa-3x"></i>
                 </a>
-            </header>
+                <a href="https://github.com/Ahsan-Ehtesham">
+                  <i className="fab fa-twitter fa-2x"></i>
+                </a>
+                <a href="https://github.com/Ahsan-Ehtesham">
+                  <i className="fab fa-linkedin-in fa-2x"></i>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
